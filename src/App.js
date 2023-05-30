2799281c07fef1199592ff23aed91c6def1b646e
gabrielmussafiri <musgabriel2@gmail.com>
2023-05-30 14:16:20 +0200

    Main component updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Components/Header";
+import Main from "./Components/Main";
 
 function App() {
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,19 @@
 function Main() {
-  return <div>
-    
-  </div>;
+  return (
+    <div>
+      <section className="section--main">
+        <div className="section--picture">
+          <img src="https://source.unsplash.com/WLxQvbMyfas" alt="" />
+        </div>
+        <div className="section--text">
+          <p>Japan</p>
+          <h3 className="section--title">Mount Fuji</h3>
+          <p>Date</p>
+          <p>lorem ipsum</p>
+        </div>
+      </section>
+      <hr className="ligne" />
+    </div>
+  );
 }
 export default Main;
